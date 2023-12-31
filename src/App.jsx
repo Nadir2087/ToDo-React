a256ad3d0a5bbd9ec3df357837af1b289753eaaf
Nadir2087 <95905993+Nadir2087@users.noreply.github.com>
2023-12-31 19:44:56 +0600

    i make beta version

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
 
 function App() {
-
+  const [Tap , setTap] = useState('')
+  const [list , setlist] = useState(['hello', 'my'])
+  const addElement = (elem)=>{
+    const newArray = [...list, elem]
+    setlist(newArray)
+    setTap('')
+  }
+  
+  // let list = ['Lorem ipsum dolor sit amet.', "Lorem ipsum dolor sit amet." ,"Lorem ipsum dolor sit amet." ,"Lorem ipsum dolor sit amet."]
   return (
     <>
-    
+      <main>
+        <input type="text" placeholder="Enter new Tap" onChange={(event)=>setTap(event.target.value)} value={Tap}/>
+        <button onClick={()=>addElement(Tap)} >Добавить</button>
+        <section>
+          <ul>
+            {/* {list.map((lis, index)=> (<li key={index}>{lis}</li>))} */}
+            {list.map((el, index) => <li key={index}>{el}</li>)}
+          </ul>
+        </section>
+      </main>
     </>
   )
 }
