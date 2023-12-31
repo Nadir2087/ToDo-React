dfd9a8a0a20d057ebea5d842a36bec20e1ae45e7
Nadir2087 <95905993+Nadir2087@users.noreply.github.com>
2023-12-31 20:40:40 +0600

    style

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ function App() {
   // let list = ['Lorem ipsum dolor sit amet.', "Lorem ipsum dolor sit amet." ,"Lorem ipsum dolor sit amet." ,"Lorem ipsum dolor sit amet."]
   return (
     <>
-      <main>
-        <input type="text" placeholder="Enter new Tap" onChange={(event)=>setTap(event.target.value)} value={Tap}/>
-        <button onClick={()=>addElement(Tap)} >Добавить</button>
+      <main className="container">
+        <section className="row ">
+          <input className="col s10" type="text" placeholder="Enter new Tap" onChange={(event)=>setTap(event.target.value)} value={Tap}/>
+          <button className="waves-effect waves-light btn" onClick={()=>addElement(Tap)}>Добавить</button>
+        </section>
         <section>
           <ul>
             {/* {list.map((lis, index)=> (<li key={index}>{lis}</li>))} */}
