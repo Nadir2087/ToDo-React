8fca04812fbae3a15273dd89ffd01292343cff47
Nadir2087 <95905993+Nadir2087@users.noreply.github.com>
2024-01-05 20:29:21 +0600

    new funsion

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,23 @@ const StyleLi = styled.li({
   padding: "10px",
   margin: "5px 0",
   borderRadius: "5px",
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center'
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 });
 const StyleMain = styled.main({
-  maxWidth: '800px',
-  margin: '20px auto',
-  padding: '20px',
-  background: '#fff',
+  maxWidth: "800px",
+  margin: "20px auto",
+  padding: "20px",
+  background: "#fff",
   borderRadius: "5px",
+});
 
-})
 
 function App() {
   const [Tap, setTap] = useState("");
   const [list, setlist] = useState(["hello", "my"]);
+  const [disabled, setDisabled] = useState(true);
   const addElement = (elem) => {
     const newArray = [...list, elem];
     setlist(newArray);
@@ -36,10 +37,36 @@ function App() {
       addElement(Tap);
     }
   };
-  const handleDel = (index)=>{
-    const newArray = [...list]
+  const handleDel = (index) => {
+    const newArray = [...list];
     newArray.splice(index, 1);
-    setlist(newArray)
+    setlist(newArray);
+  };
+
+  const handleUP = (index)=>{
+    let newArray = [... list]
+    if(index === 0){
+        console.log(index);
+    }else{
+      let el = newArray[index]
+      newArray.splice(index, 1)
+      newArray.splice(index-1, 0 , el)
+      // console.log(newArray);
+      setlist(newArray)
+    }
+  }
+
+  const handleDawn = (index)=>{
+    let newArray = [... list]
+    if(index === newArray.length){
+        console.log(index);
+    }else{
+      let el = newArray[index]
+      newArray.splice(index, 1)
+      newArray.splice(index+1, 0 , el)
+      // console.log(newArray);
+      setlist(newArray)
+    }
   }
 
   return (
@@ -67,7 +94,19 @@ function App() {
             {list.map((el, index) => (
               <StyleLi key={index}>
                 <span className="">{el}</span>
-                <span onClick={()=>handleDel(index)} className="btn red lighten-2">del</span>
+                <div style={{display: 'flex'}}>
+                  <div style={{display: 'flex', alignItems: 'center',marginRight: '10px'}}>
+                    <div className={`arrow_down ${index === list.length-1? 'disabled': ''}`} onClick={()=> index === list.length-1?console.log(): handleDawn(index)}></div>
+                    <div className={`arrow_up ${index === 0? 'disabledup': ''}`} onClick={()=>index === 0? console.log(): handleUP(index)}></div>
+                  </div>
+                  
+                  <span
+                    onClick={() => handleDel(index)}
+                    className="btn red lighten-2"
+                  >
+                    del
+                  </span>
+                </div>
               </StyleLi>
             ))}
           </ul>
